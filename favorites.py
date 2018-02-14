7207b646d5e8825486ef7bed755f0f65cfa391a6
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-14 08:34:29 -0500

    Updated from Brython Server: 2/14/2018 8:34:28 AM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,9 @@
+#Heather Stafford
+#2/14/18
+#favorites.py
 
+word = input('Enter your favorite word: ')
+num = int(input('Enter your favorite number: '))
+
+for i in range(0, num):
+    print(word)
