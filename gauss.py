982c7551acd74d3b6efacdd473e65a79e97e1660
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-15 08:34:22 -0500

    Updated from Brython Server: 2/15/2018 8:34:21 AM

--- gauss.py
+++ gauss.py
@@ -1,1 +1,13 @@
+#Heather Stafford
+#2/15/18
+#gauss.py
 
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a larger number: '))
+
+total = 0
+
+for i in range(num1, num2 + 1):
+    total = total + i
+print(total)
+    
