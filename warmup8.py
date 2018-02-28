7aafcf05cd6a043be15731a6f9585bb90b577266
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-28 08:30:57 -0500

    Updated from Brython Server: 2/28/2018 8:30:56 AM

--- warmup8.py
+++ warmup8.py
@@ -1,1 +1,11 @@
+#Heather Stafford
+#2/28/18
+#warmup8.py
 
+total = 0
+for i in range(1,100000):
+    if i % 3 == 0 and i % 10 == 0 and i % 17 == 0:
+        total = total + i
+    
+
+print(total)
