66e669d6cad37ce90efa0f23d7492b0107d84a6f
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-01 08:42:27 -0500

    Updated from Brython Server: 3/1/2018 8:42:26 AM

--- perfectNumber.py
+++ perfectNumber.py
@@ -2,3 +2,21 @@
 #2/28/18
 #perfectNumber.py
 
+num = int(input('Enter a number: '))
+
+total = 0
+for i in range(1,num):
+    if num % i == 0:
+        total = total + i
+print(total)
+if total == num:
+    print('Perfect')
+else:
+    print('Not perfect')
+
+        
+        
+        
+        
+        
+        
