66847c83a96a0a4fd11b94769ab0a425af996f00
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-01 08:12:10 -0500

    Updated from Brython Server: 3/1/2018 8:12:10 AM

--- dotsDemo.py
+++ dotsDemo.py
@@ -1,1 +1,15 @@
+#Heather Stafford
+#3/1/18
+#dotsDemo.py - how to use loops with graphics
 
+from ggame import *
+
+red = Color(0xFF0000, 1)
+
+dot = CircleAsset(25, LineStyle(1,red), red)
+
+for i in range(10):
+    for j in range(12):
+        Sprite(dot,(10 + 60*i,10 + 60j)) #putting a row of dots
+
+App().run()
