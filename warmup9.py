bb94d265fd92b3bdda2103a2fe8c36bc5b372294
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-01 08:31:35 -0500

    Updated from Brython Server: 3/1/2018 8:31:35 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,11 @@
+#Heather Stafford
+#3/1/18
+#warmup9.py
 
+text = input('Enter text: ')
+
+for ch in text:
+    if ch=='i' or ch=='a' or ch=='e' or ch=='o':
+        print(ch.upper())
+    else:
+        print(ch.lower())
