5f3b76080d35e9a795c674bfcaba51e5280bc8c1
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-05 08:35:36 -0500

    Updated from Brython Server: 3/5/2018 8:35:34 AM

--- unit3Quiz.py
+++ unit3Quiz.py
@@ -2,24 +2,24 @@
 #3/5/18
 #unit3Quiz
 
-"""
 i = -15
 while i < -8:
     print(i)
     i += 1
     
+    
 for i in range(50,17,-4):
     print(i)
     
+    
 total = 0
-for i in range(-100,1000,2):
+for i in range(-100,1001,2):
     total = total + i
 print(total)
-"""
+
 
 while True:
     text = input('Enter text: ')
-    for ch in text:
-        if ch in 'alpaca':
-            break
+    if 'alpaca' in text:
+        break
     print(text)
